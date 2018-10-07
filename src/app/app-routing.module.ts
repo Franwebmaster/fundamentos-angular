a7cdaed808a79a26b811c94bc876db17bea8209f
Francisco <franciscobarros754@gmail.com>
2018-10-07 17:41:07 -0300

    erro de rota inexistente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { Routes, RouterModule } from '@angular/router';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'user/flavio', component: PhotoListComponent },
-  { path: 'user/add', component: PhotoFormComponent }
+  { path: 'user/add', component: PhotoFormComponent },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
