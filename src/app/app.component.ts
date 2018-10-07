6caeaf6cac1f40c9d5a3777b9c4314180541d1a0
Francisco <franciscobarros754@gmail.com>
2018-10-06 21:34:30 -0300

    implementando service e interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { HttpClient } from '@angular/common/http';
+import { PhotoService } from './photos/photo/photo.service';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +10,16 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   photos: Object[] = []
 
-  constructor(http: HttpClient){
-    http.get<Object[]>('http://localhost:3000/flavio/photos')
-        .subscribe(
-          photos => this.photos = photos,
-          error => console.log(error.message)
-        )
+  constructor(photoService: PhotoService){
+    photoService.listFromUser('flavio').subscribe(
+      photo => {
+        console.log(photo[0].id);
+        this.photos = photo
+        console.log(photo);
+      }
+    )
   }
+
+  
+
 }
--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,12 +1,16 @@
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
+import { Photo } from './photo';
+
+const API = 'http://localhost:3000/';
+
+@Injectable({ providedIn: 'root' })
 export class PhotoService {
 
-    constructor(http: HttpClient){
-        
-    }
+    constructor(private http: HttpClient){}
 
-    listFromuser(user) {
-            
+    listFromUser(userName: string) {
+        return this.http.get<Photo[]>(API + 'flavio/photos');
     }
 }
